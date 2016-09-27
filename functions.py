69d1ef09e7837557e7ad59826186a41334eaced8
Brett Morris <brettmorris21@gmail.com>
2016-09-27 15:27:31 -0700

    I added some spaces

--- functions.py
+++ functions.py
@@ -1,6 +1,6 @@
 # Here are some functions that you can fill in to make them work.
 # Replace the "raise NotImplementedError" line with the correct code!
-
+   
 def print_the_instructors_name():
     raise NotImplementedError
 
